4382e61cc75c49bc62292e903bf7cf2546655f8e
fanoway <86997883+fanoway@users.noreply.github.com>
2023-03-10 15:30:52 -0800

    merged backends

--- backend/src/locations.rs
+++ backend/src/locations.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate rocket;
 use common::Location;
-use rocket::fs::NamedFile;
 use serde_json::de::from_reader;
 use std::fs::File;
 use std::io;
@@ -9,6 +8,7 @@ use std::io::BufReader;
 
 fn read_json() -> Vec<Location> {
     // TODO error handling
+    // TODO transition from lpocal file to KV worker
     let json_file_path: String =
         r"C:\Users\micha\Documents\rust_yew\goldilocks\static\locations_flat.json".to_string();
     let location_file = File::open(json_file_path).unwrap();
@@ -16,19 +16,4 @@ fn read_json() -> Vec<Location> {
     let locations: Vec<Location> = from_reader(location_reader).unwrap();
 
     return locations;
-}
-
-#[get("/all")]
-fn loc_all() -> String {
-    let locations = read_json();
-
-    let j = serde_json::to_string(&locations).unwrap();
-
-    j
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/api/v1/locations", routes![loc_all])
 }
