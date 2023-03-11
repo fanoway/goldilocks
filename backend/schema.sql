689347ca1e5f322c470c0e8772f21f9b8c2d1757
fanoway <86997883+fanoway@users.noreply.github.com>
2023-03-10 18:33:07 -0800

    removed d1

--- backend/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS Locations;
-CREATE TABLE Locations (id INT, name TEXT, url TEXT, lat FLOAT, long FLOAT PRIMARY KEY (`id`));
-INSERT INTO Customers (id, name, url,lat,long) VALUES (1, 'climbing1', 'wwww.test1', -122, 45), (2, 'climbinging 2', 'www.test2', -123, 46) ;
